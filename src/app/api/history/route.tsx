f0f30f61e79197b85884415ce46c3d705be746d4
cesarlojan8 <cesarlojan8@gmail.com>
2025-05-29 22:57:47 +0000

    asdf

--- src/app/api/history/route.tsx
+++ src/app/api/history/route.tsx
@@ -2,8 +2,8 @@ import {query } from '@/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest){
-    let test:string[] = [""]
-    const rows = await query('select * from history')
+    
+    const rows = await query('SELECT * FROM history')
     console.log(rows)
     return NextResponse.json({rows:rows})
 }
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,18 @@
-import mysql from "mysql2"
-
-export async function query(query:string){
-    const connection = mysql.createConnection({
-        host:'mx130.hostgator.mx',        
-        user:'jeussval_admin',
-        password:'Admin@234',        
-        database: 'jeussval_aquadb', 
-    })
+import mysql from "mysql2/promise"
+const connection = mysql.createPool({
+    host:'mx130.hostgator.mx',        
+    user:'jeussval_admin',
+    password:'Admin@234',        
+    database: 'jeussval_aquadb', 
+    waitForConnections:true,
+    connectionLimit:10,
+})
+export async function query(sql:string, values?:any[]){
+   
     try{    
-        const results = await connection.execute(query);
-        console.log("Conexion exitosa");
+        const [rows] = await connection.execute(sql,values);        
         connection.end();
-        return results
+        return rows
     }catch(error){
         console.error("Error ", error);
         return {error};
