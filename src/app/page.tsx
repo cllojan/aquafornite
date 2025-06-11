86b4949120e992460edff4821cee670a4a1c4d8e
cllojan <cesarlojan8@gmail.com>
2025-06-11 10:29:32 -0500

    cloudflare

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header';
 import Skins from '@/components/Skins';
 
 import { getSkins } from "@/lib/getSkins";
-
+export const runtime = "edge";
 export default async function Home() {
   const {skins, categories} = await getSkins();
   
