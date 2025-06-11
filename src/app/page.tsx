ca6fac72dc92c7ffe2b1ba574e39f92986b85a67
cesarlojan8 <cesarlojan8@gmail.com>
2025-06-11 17:24:18 +0000

    remove runtima

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-
-
 import Header from '@/components/Header';
 import Skins from '@/components/Skins';
-
 import { getSkins } from "@/lib/getSkins";
 
 export default async function Home() {
-  const {skins, categories} = await getSkins();
-  
+  const {skins, categories} = await getSkins();  
   return (
     <div className="">                
       <Header/>
--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-export const runtime = "edge";
+
 export default function Page(){
     return (
         <div className="w-full h-screen flex justify-center align-center">
